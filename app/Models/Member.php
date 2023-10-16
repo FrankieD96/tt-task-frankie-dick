07615010b10593bf26648fcbd535ef118d9ded66
Frankie Dick <fhd96@hotmail.co.uk>
2023-10-16 20:41:49 +0100

    Created Member + School Models, migrations, controllers and Routes

---
+++ app/Models/Member.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Member extends Model
+{
+    use HasFactory;
+
+    public function schools(): BelongsToMany
+    {
+        return $this->belongsToMany(School::class);
+    }
+}
