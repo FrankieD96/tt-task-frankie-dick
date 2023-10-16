f0b1ca18e632a15bda66b767ad981690f14526e8
Frankie Dick <fhd96@hotmail.co.uk>
2023-10-16 21:29:31 +0100

    set json response for getallSchools

--- app/Http/Controllers/SchoolController.php
+++ app/Http/Controllers/SchoolController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\School;
 use Illuminate\Http\Request;
 
 class SchoolController extends Controller
 {
     public function getAllSchools()
     {
-        
+        return response()->json([
+            'data' => School::all()->makeHidden(['created_at', 'updated_at']),
+            'message' => 'Schools succesfully retrieved'
+        ]);
     }
 }
