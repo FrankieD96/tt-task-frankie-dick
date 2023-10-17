caeffbe9c32f944551dac37f779a4e6044ed986f
Frankie Dick <fhd96@hotmail.co.uk>
2023-10-17 12:16:40 +0100

    adding return type hints to controllers

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Models\Member;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class MemberController extends Controller
 {
-    public function getAllMembers()
+    public function getAllMembers(): JsonResponse
     {
         return response()->json([
             'data' => Member::with('schools:id,name')->get()->makeHidden(['created_at', 'updated_at']),
--- app/Http/Controllers/SchoolController.php
+++ app/Http/Controllers/SchoolController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Models\School;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class SchoolController extends Controller
 {
-    public function getAllSchools()
+    public function getAllSchools(): JsonResponse
     {
         return response()->json([
             'data' => School::all()->makeHidden(['created_at', 'updated_at']),
